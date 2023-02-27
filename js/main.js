395e19bdc3b1047444ad9373d034eb9ff6476d01
Sebastian Saavedra <sebasasaavedra@gmail.com>
2023-02-27 18:23:12 -0300

    primer commit cv

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("hola mundo")
+
+let miBoton=document.getElementById("miBoton")
+
+miBoton.addEventListener("click",saludar);
+
+function saludar(){
+    alert("El msj se ha enviado")
+}
+
+
